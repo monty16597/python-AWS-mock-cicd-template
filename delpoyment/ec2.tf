5ad2c232b456868acd9c2eb35931ce80dfc63661
monty16597 <alonjamohit77@gmail.com>
2024-03-24 15:25:03 -0400

    Added healthcheck

--- delpoyment/ec2.tf
+++ delpoyment/ec2.tf
@@ -25,19 +25,37 @@ resource "aws_security_group" "ec2" {
   name = "ec2"
   vpc_id = aws_vpc.vpc.id
 
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
-
     egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     }
+
+    ingress = [ 
+        {
+            from_port = 80
+            to_port = 80
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "Allow HTTP inbound traffic"
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = true
+        },
+        {
+            from_port = 22
+            to_port = 22
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "Allow SSH traffic"
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
+            security_groups = []
+            self = true
+        }
+    ]
 }
 
 
