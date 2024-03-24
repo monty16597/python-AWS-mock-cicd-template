0958a9842342e01c619bc3fe62f668c1989118d4
monty16597 <alonjamohit77@gmail.com>
2024-03-24 14:58:33 -0400

    Added system blueprint

--- app/blueprints/system.py
+++ app/blueprints/system.py
@@ -1,13 +1,13 @@
 from flask import Blueprint
 
-main_bp = Blueprint("main", __name__)
+system_bp = Blueprint("main", __name__)
 
 
-@main_bp.route("/", methods=["GET"])
+@system_bp.route("/", methods=["GET"])
 def index():
     return "Use post request to /generate_pdf to generate a PDF file", 200
 
 
-@main_bp.route("/health", methods=["GET"])
+@system_bp.route("/health", methods=["GET"])
 def health():
     return "OK", 200
